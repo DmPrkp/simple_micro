a40b9b5a1d484dbf27083dff8e137ca829639b22
Dmitriy Prokopovich <prokopovich@indins.ru>
2021-08-27 20:36:49 +0300

    added event buss

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 export default () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:4000/posts');
-
+    const res = await axios.get('http://localhost:4002/posts');
     setPosts(res.data);
   };
 
@@ -18,11 +19,13 @@ export default () => {
     return (
       <div
         className="card"
-        style={{ width: '30%', marginBottom: '20px' }}
+        style={{ width: '30%', marginBottom: '10px' }}
         key={post.id}
       >
         <div className="card-body">
-          <h3>{post.title}</h3>
+          <h4>{post.title}</h4>
+          <CommentList comments={post.comments}/>
+          <CommentCreate postId={post.id}/>
         </div>
       </div>
     );
