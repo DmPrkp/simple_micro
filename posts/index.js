7a5062fe24cfb64c21e02c1e767bc3544e590541
Dmitriy Prokopovich <prokopovich@indins.ru>
2021-09-07 20:46:39 +0300

    add moderation

--- posts/index.js
+++ posts/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
-const axios = require('axios')
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -26,17 +26,19 @@ app.post('/posts', async (req, res) => {
   await axios.post('http://localhost:4005/events', {
     type: 'PostCreated',
     data: {
-      id, title
+      id,
+      title
     }
-  })
+  });
 
   res.status(201).send(posts[id]);
 });
 
-app.post('/events', (req, res)=> {
+app.post('/events', (req, res) => {
   console.log('Received Event', req.body.type);
-  res.send({})
-})
+
+  res.send({});
+});
 
 app.listen(4000, () => {
   console.log('Listening on 4000');
